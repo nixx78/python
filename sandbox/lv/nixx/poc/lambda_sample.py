719b86eacc0da9ee52c52b30c871b66027b8c7c4
nixx78 <nixx78@gmail.com>
2018-12-28 20:53:29 +0200

    More samples added

--- sandbox/lv/nixx/poc/lambda_sample.py
+++ sandbox/lv/nixx/poc/lambda_sample.py
@@ -1,3 +1,11 @@
 s = lambda a, b: a + b
 
 print(s(1, 2))
+
+
+def mapper(a): return a + 'mapped'
+
+lst = ['1', '2', '3', '4', '5']
+
+mappedLst = map(mapper, lst)
+print('Mapped list:', ' '.join(mappedLst))
